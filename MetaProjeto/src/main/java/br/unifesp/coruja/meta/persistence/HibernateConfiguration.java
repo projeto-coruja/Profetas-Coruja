db7f7f40509e006cc175cbc3509d3757e8e88f79
Daniel Gracia <hueho@sidad-MS-7680>
2012-09-17 15:34:41 -0300

    Corrigindo acesso ao banco.

--- MetaProjeto/src/main/java/br/unifesp/coruja/meta/persistence/HibernateConfiguration.java
+++ MetaProjeto/src/main/java/br/unifesp/coruja/meta/persistence/HibernateConfiguration.java
@@ -4,12 +4,12 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate3.HibernateTransactionManager;
-import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 @Configuration
 public class HibernateConfiguration {
@@ -18,24 +18,16 @@ public class HibernateConfiguration {
 	private DataSource dataSource;
 
 	@Bean
-	public AnnotationSessionFactoryBean sessionFactoryBean() {
+	public LocalSessionFactoryBean sessionFactoryBean() {
 		Properties props = new Properties();
-		props.put("hibernate.dialect", H2Dialect.class.getName());
+		props.put("hibernate.dialect", PostgreSQL82Dialect.class.getName());
 		props.put("hibernate.format_sql", "true");
-		
-		
-//		props.put("hibernate.hbm2ddl.auto", "create-drop");
-//		props.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-//	    props.put("hibernate.connection.username", "coruja");
-//	    props.put("hibernate.connection.password", "coruja");
-//	    props.put("hibernate.connection.url", "jdbc:mysql://localhost:5432/coruja_metaprojeto");
+		props.put("hibernate.hbm2ddl.auto", "create-drop");
 
-	    
-		AnnotationSessionFactoryBean bean = new AnnotationSessionFactoryBean();
+	    LocalSessionFactoryBean bean = new LocalSessionFactoryBean();
 		bean.setAnnotatedClasses(new Class[]{Item.class, Order.class});		
 		bean.setHibernateProperties(props);
 		bean.setDataSource(this.dataSource);
-		bean.setSchemaUpdate(true);
 		return bean;
 	}
 
