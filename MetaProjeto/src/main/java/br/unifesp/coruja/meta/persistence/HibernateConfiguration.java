5e2f60315b827502f45fd1ecc2b727ae24bfeeb4
Daniel Gracia <hueho@sidad-MS-7680>
2012-09-19 18:02:54 -0300

    Adicionando o uso de Properties e iniciando tabelas User e Role no metaprojeto não-separado (exportar para o metaprojeto modular no futuro).

--- MetaProjeto/src/main/java/br/unifesp/coruja/meta/persistence/HibernateConfiguration.java
+++ MetaProjeto/src/main/java/br/unifesp/coruja/meta/persistence/HibernateConfiguration.java
@@ -1,52 +1,49 @@
 package br.unifesp.coruja.meta.persistence;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.hibernate.dialect.PostgreSQL82Dialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
-//import org.springframework.beans.factory.annotation.Value;
-
 @Configuration
 public class HibernateConfiguration {
 
-	// @Value("#{dataSource}")
 	private DataSource dataSource;
 
-	private DataSource getDataSource() {
+	private DataSource getDataSource(Properties props) {
 		if (dataSource == null) {
 			DriverManagerDataSource newDataSource = new DriverManagerDataSource();
-			newDataSource.setDriverClassName("org.postgresql.Driver");
-			newDataSource.setUrl("jdbc:postgresql://localhost:5432/coruja_metaprojeto");
-			newDataSource.setUsername("coruja");
-			newDataSource.setPassword("coruja");
+			newDataSource.setDriverClassName(props.getProperty("jdbc.driverClassName"));
+			newDataSource.setUrl(props.getProperty("jdbc.url"));
+			newDataSource.setUsername(props.getProperty("jdbc.username"));
+			newDataSource.setPassword(props.getProperty("jdbc.password"));
 			this.dataSource = newDataSource;
 		}
 		return this.dataSource;
 	}
 
 	@Bean
-	public LocalSessionFactoryBean sessionFactoryBean() {
-		Properties props = new Properties();
-		props.put("hibernate.dialect", PostgreSQL82Dialect.class.getName());
-		props.put("hibernate.format_sql", "true");
-		props.put("hibernate.hbm2ddl.auto", "create-drop");
+	public LocalSessionFactoryBean sessionFactoryBean() throws IOException {
+		Properties jdbc_props = PropertiesLoaderUtils.loadProperties(new ClassPathResource("/jdbc.properties"));
+		Properties hibernate_props = PropertiesLoaderUtils.loadProperties(new ClassPathResource("/hibernate.properties"));
 		
 		LocalSessionFactoryBean bean = new LocalSessionFactoryBean();
-		bean.setAnnotatedClasses(new Class[] { Item.class, Order.class });
-		bean.setHibernateProperties(props);
-		bean.setDataSource(getDataSource());
+		bean.setAnnotatedClasses(new Class[] { Item.class, Order.class, User.class, Role.class });
+		bean.setHibernateProperties(hibernate_props);
+		bean.setDataSource(getDataSource(jdbc_props));
 		return bean;
 	}
 
 	@Bean
-	public HibernateTransactionManager transactionManager() {
+	public HibernateTransactionManager transactionManager() throws IOException {
 		return new HibernateTransactionManager(sessionFactoryBean().getObject());
 	}
 
