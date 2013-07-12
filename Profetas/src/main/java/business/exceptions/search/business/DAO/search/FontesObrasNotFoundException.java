26ce52c7d366806e5d3557c913fab0ca766e2022
jenniferlins <jenniferlins@sidad-MS-7680>
2013-07-12 09:55:44 -0300

    correção de bug.

---
+++ Profetas/src/main/java/business/exceptions/search/business/DAO/search/FontesObrasNotFoundException.java
@@ -0,0 +1,10 @@
+package business.exceptions.search.business.DAO.search;
+
+public class FontesObrasNotFoundException extends Exception {
+
+	public FontesObrasNotFoundException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ Profetas/src/test/java/business/Bean/search/FontesObrasSearchTest.java
@@ -0,0 +1,67 @@
+package business.Bean.search;
+
+import static org.junit.Assert.*;
+
+
+import java.util.List;
+
+import org.junit.Test;
+
+import persistence.dto.Classificacao;
+import persistence.dto.DTO;
+import persistence.dto.FontesObras;
+import persistence.dto.GrupoMovimento;
+import business.DAO.search.ClassificacaoSearchDAO;
+import business.DAO.search.FontesObrasSearchDAO;
+import business.DAO.search.GrupoMovimentoSearchDAO;
+import business.exceptions.login.UnreachableDataBaseException;
+import business.exceptions.model.ClassificationNotFoundException;
+import business.exceptions.model.GroupMovementNotFoundException;
+import business.exceptions.search.business.DAO.search.FontesObrasNotFoundException;
+
+
+public class FontesObrasSearchTest {
+
+	//@Test
+	public void findByclassificacaotest() throws UnreachableDataBaseException, FontesObrasNotFoundException, ClassificationNotFoundException {
+		FontesObrasSearchDAO dao = new FontesObrasSearchDAO();//pesquisa pela fonte/obra
+		ClassificacaoSearchDAO dao2 = new ClassificacaoSearchDAO();
+		
+		DTO tipo  = dao2.findExactClassificacao("romance");
+	
+		
+		List<DTO> lalala = dao.findFontesObrasByClassificacao(tipo.getId());
+		for(DTO p : lalala){
+			System.out.println(((FontesObras)p).getTitulo());
+		}
+		
+	}
+	@Test
+	public void findBygrupoMovimentotest() throws UnreachableDataBaseException, FontesObrasNotFoundException, ClassificationNotFoundException, GroupMovementNotFoundException {
+		FontesObrasSearchDAO dao = new FontesObrasSearchDAO();//pesquisa pela fonte/obra
+		GrupoMovimentoSearchDAO dao2 = new GrupoMovimentoSearchDAO();
+		
+		DTO tipo  = (DTO) dao2.findExactGrupoMovimentoByNome("grupo");
+		//List<DTO> grupo =  dao2.findAllGrupoMovimento();
+		//for(DTO g : grupo){
+		System.out.println(tipo.getId());
+		//}
+		
+		
+		List<DTO> lalala = dao.findFontesObrasByGrupoMovimento(tipo.getId());
+		for(DTO p : lalala){
+			System.out.println(((FontesObras)p).getTitulo());
+		}
+		
+	}
+	//@Test
+	public void findAlltest() throws UnreachableDataBaseException, FontesObrasNotFoundException, ClassificationNotFoundException {
+		FontesObrasSearchDAO dao = new FontesObrasSearchDAO();//pesquisa pela fonte/obra	
+		List<DTO> lalala = dao.findAllFontesObras();
+		for(DTO p : lalala){
+			System.out.println(((FontesObras)p).getTitulo());
+		}
+		
+	}
+
+}
